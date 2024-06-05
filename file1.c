7d64ad5a5caae7ed3ebec600f60756d4ea78b6c7
Hao-yiwen <1552975589@qq.com>
2024-06-05 10:16:50 +0800

    feat: add cmake build

---
+++ file1.c
@@ -0,0 +1,14 @@
+//
+// Created by 郝宜文 on 2024/6/5.
+//
+
+#include <stdio.h>
+#include "file1.h"
+
+void hello(){
+    printf("Hello, World!\n");
+}
+
+int add(int a, int b){
+    return a + b;
+}
---
+++ file1.h
@@ -0,0 +1,11 @@
+//
+// Created by 郝宜文 on 2024/6/5.
+//
+
+#ifndef C_DEMO_FILE1_H
+#define C_DEMO_FILE1_H
+
+void hello(void);
+int add(int a, int b);
+
+#endif //C_DEMO_FILE1_H
